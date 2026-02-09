9c507e2c5608384d38d5057d2503f5dacdca61a3
naeem0172 <markwilliamson248@gmail.com>
2026-02-09 12:51:15 +0600

    Add Supabase client initialization script

---
+++ js/supabase.js
@@ -0,0 +1,10 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+<script>
+  const supabaseUrl = "https://fehcawwhsynckfjwbw.supabase.co";
+  const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlaG5jY2F3eWhzeW5ja2Zqd2J3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2MDI5MjksImV4cCI6MjA4NjE3ODkyOX0.eLcitUKdLTmHE4wnq7uTdGnzuRnuMhyNAFSau9qNflk";
+
+  const supabase = window.supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+  );
+</script>
